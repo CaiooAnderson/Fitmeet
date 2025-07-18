2cd52f1391b35ff0cf34db6eed62ca9eea7162b0
Caio Anderson <caioandersongoes@gmail.com>
2025-07-18 13:51:45 -0300

    Changed getActivityTypes

--- backend/src/services/activityService.ts
+++ backend/src/services/activityService.ts
@@ -6,7 +6,7 @@ import {
   getDefaultAvatarUrl,
   getSignedAvatarUrl,
   uploadImage,
-  bucketName
+  bucketName,
 } from "./s3Service";
 import prisma from "../orm/database";
 import { generateToken } from "../middlewares/authMiddleware";
@@ -54,7 +54,21 @@ export const determineUserSubscriptionStatus = async (
 };
 
 const getActivityTypes = async () => {
-  return await ActivityRepository.getActivityTypes();
+  const types = await ActivityRepository.getActivityTypes();
+
+  const typesWithSignedUrl = await Promise.all(
+    types.map(async (type) => {
+      // Se o campo 'image' armazenar a key no bucket (ex: 'activities/gym.png')
+      const signedUrl = type.image ? await getSignedAvatarUrl(type.image) : null;
+
+      return {
+        ...type,
+        image: signedUrl,
+      };
+    })
+  );
+
+  return typesWithSignedUrl;
 };
 
 const listActivities = async (userId: string, filters: any) => {
