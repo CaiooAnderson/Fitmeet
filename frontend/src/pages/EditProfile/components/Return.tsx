6b3ef26eae32736a7a05f8845e793b7bcf8b3d76
Caio Anderson <caioandersongoes@gmail.com>
2025-08-04 13:41:49 -0300

    Added effects in EditProfile Return hover action

--- frontend/src/pages/EditProfile/components/Return.tsx
+++ frontend/src/pages/EditProfile/components/Return.tsx
@@ -6,10 +6,10 @@ export default function Return() {
 
   return (
     <div
-      className="w-fit flex items-center justify-start h-6 border-0 shadow-none gap-2 cursor-pointer"
+      className="w-fit flex items-center justify-start h-6 gap-2 cursor-pointer transition-all duration-300 hover:text-primary-600 group"
       onClick={() => navigate("/profile")}
     >
-      <ChevronLeft className="w-5 h-5" />
+      <ChevronLeft className="w-5 h-5 transition-transform duration-300 group-hover:-translate-x-1" />
       <span className="text-[1rem] font-bold">Voltar para o perfil</span>
     </div>
   );
