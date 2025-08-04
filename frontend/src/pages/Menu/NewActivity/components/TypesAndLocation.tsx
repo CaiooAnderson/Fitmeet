1102cfdd24a540090e30031c8025a5e769784966
Caio Anderson <caioandersongoes@gmail.com>
2025-08-04 14:19:28 -0300

    Added ellipsis on Activities Types on NewActivity

--- frontend/src/pages/Menu/NewActivity/components/TypesAndLocation.tsx
+++ frontend/src/pages/Menu/NewActivity/components/TypesAndLocation.tsx
@@ -62,12 +62,12 @@ export default function TypesAndLocation({
                     <img
                       src={type.image?.replace("localstack", "localhost")}
                       alt={type.name}
-                      className={`w-20 h-20  rounded-full object-cover text-ellipsis overflow-hidden max-w-full border-2 box-content transition duration-300
+                      className={`w-20 h-20  rounded-full object-cover border-2 box-content transition duration-300
                   ${isSelected ? "border-primary" : "border-transparent"}
                   group-hover:brightness-75
                 `}
                     />
-                    <p className="text-[1rem] text-center font-semibold h-5 leading-none">
+                    <p className="text-[1rem] text-center font-semibold h-5 leading-none text-ellipsis overflow-hidden max-w-full">
                       {type.name}
                     </p>
                   </div>
