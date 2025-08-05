fe66529f7899bf095dc3ee9eabb4ac4ea9b1e5d5
Caio Anderson <caioandersongoes@gmail.com>
2025-08-05 17:38:37 -0300

    Fix on marquee effect

--- frontend/src/pages/Profile/components/ProfileUserInfo.tsx
+++ frontend/src/pages/Profile/components/ProfileUserInfo.tsx
@@ -71,9 +71,9 @@ export default function ProfileUserInfo({ user }: ProfileUserInfoProps) {
         <h1 ref={containerRef} className="relative text-[2rem] font-bebas overflow-hidden w-48 text-center">
           <span
             ref={textRef}
-            className={isOverflowing ? "marquee inline-block" : "inline-block"}
+            className={isOverflowing ? "title-marquee inline-block" : "inline-block"}
           >
-            {user.name}
+            &nbsp;{user.name}
           </span>
         </h1>
       </div>
