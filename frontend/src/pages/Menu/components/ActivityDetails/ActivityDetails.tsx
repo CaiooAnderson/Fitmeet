047ea9e79d5dc120114458083987054f1afc7bf8
Caio Anderson <caioandersongoes@gmail.com>
2025-08-06 16:39:49 -0300

    Added marquee effect on activity title on creator and participant modal

--- frontend/src/pages/Menu/components/ActivityDetails/ActivityDetails.tsx
+++ frontend/src/pages/Menu/components/ActivityDetails/ActivityDetails.tsx
@@ -217,8 +217,20 @@ export default function ActivityDetails({
                 src={activity.image?.replace("localstack", "localhost")}
                 className="h-56 w-full object-cover rounded-lg mb-6"
               />
-              <h2 className="text-[2rem] h-9 mb-2 font-bebas overflow-hidden text-ellipsis">
-                {activity.title}
+              <h2 className="text-[2rem] h-9 mb-2 font-bebas overflow-hidden">
+                <span
+                  className="inline-block"
+                  style={{
+                    whiteSpace: "nowrap",
+                    minWidth: "100%",
+                    animation:
+                      activity.title.length > 30
+                        ? "marquee 8s linear infinite"
+                        : "none",
+                  }}
+                >
+                  {activity.title}
+                </span>
               </h2>
               <p className="text-[1rem] h-36 text-gray-700 mb-6 whitespace-normal overflow-y-auto">
                 {activity.description}
