886d70ba9657fdca5a6387994dd473efe01ed720
Caio Anderson <caioandersongoes@gmail.com>
2025-08-11 15:00:20 -0300

    Fix Responsivity in Recommended on Menu.tsx

--- frontend/src/pages/Menu/components/Recommended.tsx
+++ frontend/src/pages/Menu/components/Recommended.tsx
@@ -81,13 +81,6 @@ const Recommended = ({
   );
 
   const listToShow = filteredActivities.slice(0, itemsToShowCount);
-  const half = Math.ceil(listToShow.length / 2);
-  const firstLine = listToShow.slice(0, half);
-  const secondLine = listToShow.slice(half, listToShow.length);
-
-  let cardWidthClass = "w-full";
-  if (itemsToShowCount === 4) cardWidthClass = "w-1/2";
-  else if (itemsToShowCount === 8) cardWidthClass = "w-1/4";
 
   const handleActivityClick = (activity: any) => {
     setSelectedActivity(activity);
@@ -101,7 +94,9 @@ const Recommended = ({
   return (
     <div className="w-full flex flex-col">
       <div className="flex justify-between items-center h-8 mb-4">
-        <h2 className="text-[1.75rem] font-bebas">{title ?? "RECOMENDADO PARA VOCÊ"}</h2>
+        <h2 className="text-[1.75rem] font-bebas">
+          {title ?? "RECOMENDADO PARA VOCÊ"}
+        </h2>
       </div>
 
       {error && (
@@ -117,73 +112,45 @@ const Recommended = ({
               Nenhuma atividade de sua preferência disponível no momento.
             </div>
           ) : (
-            <>
-              <div className="flex flex-wrap gap-3">
-                {firstLine.map((activity) => (
-                  <div
-                    key={activity.id}
-                    className={`flex flex-col items-start cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap p-2 ${cardWidthClass}`}
-                    onClick={() => handleActivityClick(activity)}
-                  >
-                    <div className="relative w-full h-40 mb-4 rounded-lg overflow-hidden">
-                      <img
-                        src={activity.image?.replace("localstack", "localhost")}
-                        className="w-full h-full object-cover rounded-lg"
-                      />
-                      {activity.private && (
-                        <div className="absolute top-1 left-1 p-[6px] bg-gradient-to-b from-[#00BC7D] to-[#009966] rounded-full">
-                          <Lock className="text-white w-[16px] h-[16px]" />
-                        </div>
-                      )}
-                    </div>
-                    <p className="text-sm font-semibold text-left overflow-hidden text-ellipsis max-w-full">
-                      {activity.title}
-                    </p>
-                    <div className="flex items-center gap-3 text-xs text-[#404040] mt-3 h-5">
-                      <Calendar className="w-4 h-4 text-[#009966]" />
-                      {format(new Date(activity.scheduledDate), "dd/MM/yyyy HH:mm")}
-                      <span className="mx-1">|</span>
-                      <Users className="w-4 h-4 text-[#009966]" />
-                      {activity.participantCount ?? 0}
-                    </div>
+            <div
+              className={`
+                grid gap-3
+                ${itemsToShowCount === 2 ? "grid-cols-1 grid-rows-2" : ""}
+                ${itemsToShowCount === 4 ? "grid-cols-2 grid-rows-2" : ""}
+                ${itemsToShowCount === 8 ? "grid-cols-4 grid-rows-2" : ""}
+                h-[33.5rem] overflow-y-auto
+              `}
+            >
+              {listToShow.map((activity) => (
+                <div
+                  key={activity.id}
+                  className="flex flex-col items-start cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap p-2"
+                  onClick={() => handleActivityClick(activity)}
+                >
+                  <div className="relative w-full h-40 mb-4 rounded-lg overflow-hidden">
+                    <img
+                      src={activity.image?.replace("localstack", "localhost")}
+                      className="w-full h-full object-cover rounded-lg"
+                    />
+                    {activity.private && (
+                      <div className="absolute top-1 left-1 p-[6px] bg-gradient-to-b from-[#00BC7D] to-[#009966] rounded-full">
+                        <Lock className="text-white w-[16px] h-[16px]" />
+                      </div>
+                    )}
                   </div>
-                ))}
-              </div>
-
-              {secondLine.length > 0 && (
-                <div className="flex flex-wrap gap-3 mt-8">
-                  {secondLine.map((activity) => (
-                    <div
-                      key={activity.id}
-                      className={`flex flex-col items-start cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap p-2 ${cardWidthClass}`}
-                      onClick={() => handleActivityClick(activity)}
-                    >
-                      <div className="relative w-full h-40 mb-4 rounded-lg overflow-hidden">
-                        <img
-                          src={activity.image?.replace("localstack", "localhost")}
-                          className="w-full h-full object-cover rounded-lg"
-                        />
-                        {activity.private && (
-                          <div className="absolute top-1 left-1 p-[6px] bg-emerald-500 rounded-full">
-                            <Lock className="text-white w-[16px] h-[16px]" />
-                          </div>
-                        )}
-                      </div>
-                      <p className="text-sm font-semibold text-left overflow-hidden text-ellipsis max-w-full">
-                        {activity.title}
-                      </p>
-                      <div className="flex items-center gap-3 text-xs text-gray-500 mt-3 h-5">
-                        <Calendar className="w-4 h-4 text-[#009966]" />
-                        {format(new Date(activity.scheduledDate), "dd/MM/yyyy HH:mm")}
-                        <span className="mx-1">|</span>
-                        <Users className="w-4 h-4 text-[#009966]" />
-                        {activity.participantCount ?? 0}
-                      </div>
-                    </div>
-                  ))}
+                  <p className="text-sm font-semibold text-left overflow-hidden text-ellipsis max-w-full">
+                    {activity.title}
+                  </p>
+                  <div className="flex items-center gap-3 text-xs text-[#404040] mt-3 h-5">
+                    <Calendar className="w-4 h-4 text-[#009966]" />
+                    {format(new Date(activity.scheduledDate), "dd/MM/yyyy HH:mm")}
+                    <span className="mx-1">|</span>
+                    <Users className="w-4 h-4 text-[#009966]" />
+                    {activity.participantCount ?? 0}
+                  </div>
                 </div>
-              )}
-            </>
+              ))}
+            </div>
           )}
 
           {selectedActivity && (
