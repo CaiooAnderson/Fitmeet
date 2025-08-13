881bed1a4c768a3006fd0b951c5c8d614a493763
Caio Anderson <caioandersongoes@gmail.com>
2025-08-13 12:02:27 -0300

    Fix on ProfileUserInfo 320px Level and Achievements block

--- frontend/src/pages/Profile/components/ProfileUserInfo.tsx
+++ frontend/src/pages/Profile/components/ProfileUserInfo.tsx
@@ -95,19 +95,15 @@ export default function ProfileUserInfo({ user }: ProfileUserInfoProps) {
 
       <div className="flex flex-col sm:flex-row gap-3 items-center sm:items-start mt-10 w-full max-w-[850px] justify-center">
         <div className="flex flex-col gap-8 bg-[#f5f5f5] rounded-lg px-8 py-8.5 w-full sm:w-[420px] h-52">
-          <div
-            className="flex justify-between items-start sm:items-center
-  [@media(max-width:320px)]:flex-row [@media(max-width:320px)]:justify-center [@media(max-width:320px)]:items-center [@media(max-width:320px)]:gap-4"
-          >
-            <div className="flex flex-col items-center gap-1">
-              <span className="text-[0.75rem] font-semibold text-[var(--text)]">
+          <div className="flex justify-between items-start sm:items-center [@media(max-width:320px)]:flex-row [@media(max-width:320px)]:justify-center [@media(max-width:320px)]:items-center [@media(max-width:320px)]:gap-4">
+            <div className="flex flex-col gap-2 [@media(max-width:320px)]:items-center [@media(max-width:320px)]:gap-1">
+              <span className="text-[0.75rem] h-[15px] font-semibold text-[var(--text)]">
                 Seu nível é
               </span>
-              <span className="text-[1.5rem] font-bold text-[var(--title)] [@media(max-width:320px)]:text-[1.25rem]">
+              <span className="text-[1.5rem] h-7.5 font-bold text-[var(--title)] [@media(max-width:320px)]:text-[1.25rem]">
                 {user.level}
               </span>
             </div>
-
             <img
               src="/Level.png"
               alt="Ícone de nível"
