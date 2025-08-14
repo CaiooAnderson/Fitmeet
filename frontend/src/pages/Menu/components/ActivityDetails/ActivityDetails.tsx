f7fac66ee39544ac76822c3b53082ff2ce6335a8
Caio Anderson <caioandersongoes@gmail.com>
2025-08-14 10:18:21 -0300

    Fix in Mobile ActivityModal on ActivityDetails

--- frontend/src/pages/Menu/components/ActivityDetails/ActivityDetails.tsx
+++ frontend/src/pages/Menu/components/ActivityDetails/ActivityDetails.tsx
@@ -202,21 +202,21 @@ export default function ActivityDetails({
 
   useEffect(() => {
     const checkTitleOverflow = () => {
-      if (titleRef.current) {
-        const limit = window.innerWidth >= 640 ? 384 : 320;
-        const isOverflowing = titleRef.current.scrollWidth > limit;
-        setCanMarqueeTitle(isOverflowing);
-      }
+      if (!titleRef.current) return;
+
+      const availableWidth =
+        window.innerWidth >= 640
+          ? titleRef.current.parentElement?.offsetWidth || 0
+          : window.innerWidth * 0.8;
+
+      const isOverflowing = titleRef.current.scrollWidth > availableWidth;
+      setCanMarqueeTitle(isOverflowing);
     };
 
-    const timeout = setTimeout(checkTitleOverflow, 100);
-
+    checkTitleOverflow();
     window.addEventListener("resize", checkTitleOverflow);
 
-    return () => {
-      clearTimeout(timeout);
-      window.removeEventListener("resize", checkTitleOverflow);
-    };
+    return () => window.removeEventListener("resize", checkTitleOverflow);
   }, [activity.title]);
 
   useEffect(() => {
@@ -325,9 +325,10 @@ export default function ActivityDetails({
                   canMarqueeTitle ? "cursor-pointer" : ""
                 }`}
                 style={{
-                  width: isSmOrLarger
-                    ? "100%"
-                    : `${Math.floor(window.innerWidth * 0.8)}px`,
+                  width:
+                    window.innerWidth >= 640
+                      ? "100%"
+                      : `${window.innerWidth * 0.8}px`,
                 }}
                 onClick={() => {
                   if (canMarqueeTitle) {
