e8be3913d5fe45958225f4ba07651b27e74a3576
Caio Anderson <caioandersongoes@gmail.com>
2025-08-18 09:36:43 -0300

    Added Vertical Space on Activity Modal in SubscribeActivity and ActivityDetails

--- frontend/src/pages/Menu/components/SubscribeActivity/SubscribeActivity.tsx
+++ frontend/src/pages/Menu/components/SubscribeActivity/SubscribeActivity.tsx
@@ -220,7 +220,7 @@ export default function SubscribeActivity({
       [@media(max-width:640px)]:overflow-y-auto
     "
       >
-        <div className="grid grid-cols-1 sm:grid-cols-[24rem_1fr] gap-12 w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-[24rem_1fr] gap-12 w-full py-4">
           <div className="flex flex-col justify-between h-full w-full overflow-hidden text-ellipsis whitespace-nowrap break-words">
             <div>
               <img
