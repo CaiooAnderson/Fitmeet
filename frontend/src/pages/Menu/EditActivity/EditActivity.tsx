2c4aaee0d3ecb332700127c9b392bb6523b748e8
Caio Anderson <caioandersongoes@gmail.com>
2025-08-18 14:33:37 -0300

    Fix on EditActivity modal responsivity

--- frontend/src/pages/Menu/EditActivity/EditActivity.tsx
+++ frontend/src/pages/Menu/EditActivity/EditActivity.tsx
@@ -252,7 +252,7 @@ export default function EditActivity({
             </div>
           </div>
 
-          <AlertDialogFooter className="w-full flex justify-end">
+          <AlertDialogFooter className="w-full flex sm:justify-end justify-center">
             <div className="w-full h-[48px] flex justify-end gap-4">
               <AlertDialogCancel
                 className="w-50 h-full rounded-lg text-[var(--warning)] bg-[#fff] border-1 border-[var(--warning)] hover:text-white text-sm"
