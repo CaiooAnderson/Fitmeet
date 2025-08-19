0c172857e7c8899e2ab15be9639e140fb32f836b
Caio Anderson <caioandersongoes@gmail.com>
2025-08-19 16:57:55 -0300

    Fix on Location Coordinates

--- frontend/src/pages/Menu/NewActivity/components/TypesAndLocation.tsx
+++ frontend/src/pages/Menu/NewActivity/components/TypesAndLocation.tsx
@@ -107,14 +107,13 @@ export default function TypesAndLocation({
                 })}
               />
             )}
+            {coordinates && (
+              <div className="absolute bottom-2 left-2 bg-black/60 text-white text-xs px-2 py-1 rounded-md z-[20]">
+                Lat: {coordinates.lat.toFixed(5)}, Lng:{" "}
+                {coordinates.lng.toFixed(5)}
+              </div>
+            )}
           </MapContainer>
-
-          {coordinates && (
-            <div className="absolute bottom-2 left-2 bg-black/60 text-white text-xs px-2 py-1 rounded-md z-[20]">
-              Lat: {coordinates.lat.toFixed(5)}, Lng:{" "}
-              {coordinates.lng.toFixed(5)}
-            </div>
-          )}
         </div>
       </div>
     </>
