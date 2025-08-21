6dc5103f06f6bdbce0ef0d685c7f784c656ee019
Caio Anderson <caioandersongoes@gmail.com>
2025-08-21 10:10:44 -0300

    Fixed margin in buttons on NewActivity mobile modal

--- frontend/src/pages/Menu/NewActivity/NewActivity.tsx
+++ frontend/src/pages/Menu/NewActivity/NewActivity.tsx
@@ -15,7 +15,6 @@ import Inputs from "./components/Inputs";
 import Schedule from "./components/Schedule";
 import TypesAndLocation from "./components/TypesAndLocation";
 import Approval from "./components/Approval";
-import { ChevronLeft } from "lucide-react";
 
 interface NewActivityProps {
   isOpen: boolean;
@@ -154,13 +153,7 @@ export default function NewActivity({ isOpen, onClose }: NewActivityProps) {
       [@media(max-width:640px)]:pt-[calc(env(safe-area-inset-top)+1rem)]
     "
       >
-        <button
-          onClick={onClose}
-          className="sm:hidden absolute top-6 left-6 p-2 rounded-full bg-white/90 shadow-md mt-6"
-        >
-          <ChevronLeft className="h-6 w-6 text-primary-600 bg-white/80 rounded-full" />
-        </button>
-        <div className="flex-shrink-0 [@media(max-width:640px)]:mt-4">
+        <div className="flex-shrink-0">
           <AlertDialogTitle className="text-[2rem] font-bebas font-normal">
             NOVA ATIVIDADE
           </AlertDialogTitle>
