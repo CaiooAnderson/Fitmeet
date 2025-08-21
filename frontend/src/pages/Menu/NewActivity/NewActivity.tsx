89fd5518f5346fe7666a57be2e303e5a197f59a1
Caio Anderson <caioandersongoes@gmail.com>
2025-08-21 09:48:42 -0300

    Fixed back button on NewActivity mobile modal

--- frontend/src/pages/Menu/NewActivity/NewActivity.tsx
+++ frontend/src/pages/Menu/NewActivity/NewActivity.tsx
@@ -15,6 +15,7 @@ import Inputs from "./components/Inputs";
 import Schedule from "./components/Schedule";
 import TypesAndLocation from "./components/TypesAndLocation";
 import Approval from "./components/Approval";
+import { ChevronLeft } from "lucide-react";
 
 interface NewActivityProps {
   isOpen: boolean;
@@ -150,10 +151,18 @@ export default function NewActivity({ isOpen, onClose }: NewActivityProps) {
       rounded-none sm:rounded-xl
       [@media(max-width:320px)]:min-w-[300px]
       [@media(max-width:640px)]:py-6
+      relative
     "
       >
-        <div className="flex-shrink-0">
-          <AlertDialogTitle className="text-[2rem] font-bebas font-normal">
+        <button
+          onClick={onClose}
+          className="sm:hidden absolute top-6 left-6 p-2 rounded-full bg-white/90 shadow-md"
+        >
+          <ChevronLeft className="h-6 w-6 text-primary-600 bg-white/80 rounded-full" />
+        </button>
+
+        <div className="flex-shrink-0 [@media(max-width:640px)]:mt-12">
+          <AlertDialogTitle className="text-[2rem] font-bebas font-normal text-left">
             NOVA ATIVIDADE
           </AlertDialogTitle>
           <AlertDialogDescription />
