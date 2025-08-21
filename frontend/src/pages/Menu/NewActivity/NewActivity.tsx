1d6d39e5d47d336d46cd29ddada9ac3a0458c68a
Caio Anderson <caioandersongoes@gmail.com>
2025-08-21 09:25:09 -0300

    Fixed NewActivity padding on Mobile

--- frontend/src/pages/Menu/NewActivity/NewActivity.tsx
+++ frontend/src/pages/Menu/NewActivity/NewActivity.tsx
@@ -150,7 +150,7 @@ export default function NewActivity({ isOpen, onClose }: NewActivityProps) {
       overflow-y-auto
       rounded-none sm:rounded-xl
       [@media(max-width:320px)]:min-w-[300px]
-      [@media(max-width:640px)]:py-6
+      [@media(max-width:640px)]:py-12
     "
       >
         <button
