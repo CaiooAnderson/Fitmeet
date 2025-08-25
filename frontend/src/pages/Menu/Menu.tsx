5d043c1c5a61c9209dcaa776b788403a53b24136
Caio Anderson <caioandersongoes@gmail.com>
2025-08-25 10:01:17 -0300

    Fix in user level on MenuHeader.tsx

--- frontend/src/pages/Menu/Menu.tsx
+++ frontend/src/pages/Menu/Menu.tsx
@@ -167,7 +167,7 @@ function Menu() {
       <div className="flex flex-col gap-14 w-full max-w-[76.25rem]">
         <MenuHeader
           avatar={user.avatar ?? ""}
-          level={user.level ?? 1}
+          level={user.level ?? 0}
           name={user.name ?? ""}
           onLogout={() => {
             sessionStorage.removeItem("token");
