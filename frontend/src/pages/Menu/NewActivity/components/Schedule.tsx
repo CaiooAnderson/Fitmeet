42a0c5fe633b9c7a466050801e9e017f58818d31
Caio Anderson <caioandersongoes@gmail.com>
2025-08-28 11:25:12 -0300

    Fix in Calendar for NewActivity and EditActivity on Mobile screen

--- frontend/src/pages/Menu/NewActivity/components/Schedule.tsx
+++ frontend/src/pages/Menu/NewActivity/components/Schedule.tsx
@@ -76,7 +76,7 @@ export default function Schedule({
             <CalendarIcon className="h-5 w-5" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="flex w-auto flex-col space-y-3 p-3 text-[var(--text)] z-50 [@media(max-width:640px)]:max-h-[40vh] [@media(max-width:640px)]:overflow-y-auto">
+        <PopoverContent className="flex w-auto flex-col space-y-3 p-3 text-[var(--text)] z-50 [@media(max-width:640px)]:max-h-[40vh]">
           <Select
             onValueChange={(value) => {
               const baseDate = addDays(new Date(), parseInt(value));
@@ -102,9 +102,7 @@ export default function Schedule({
             </SelectContent>
           </Select>
 
-          <div
-            className="rounded-lg border w-full"
-          >
+          <div className="rounded-lg border w-full [@media(max-width:640px)]:overflow-y-auto">
             <Calendar
               mode="single"
               selected={selectedDate}
