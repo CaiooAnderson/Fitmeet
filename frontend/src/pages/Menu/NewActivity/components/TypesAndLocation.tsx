b1f78a3dfc7347a8f7f965cb0b31dcb5335e426c
Caio Anderson <caioandersongoes@gmail.com>
2025-08-28 17:09:30 -0300

    Fix in Location coordinates when selected a place on TypesAndLocation.tsx

--- frontend/src/pages/Menu/NewActivity/components/TypesAndLocation.tsx
+++ frontend/src/pages/Menu/NewActivity/components/TypesAndLocation.tsx
@@ -110,7 +110,7 @@ export default function TypesAndLocation({
           </MapContainer>
 
           {coordinates && (
-            <div className="absolute bottom-2 left-2 bg-black/60 text-white text-xs px-2 py-1 rounded-md z-[20]">
+            <div className="absolute bottom-2 left-2 bg-black/60 text-white text-xs px-2 py-1 rounded-md pointer-events-none">
               Lat: {coordinates.lat.toFixed(5)}, Lng:{" "}
               {coordinates.lng.toFixed(5)}
             </div>
